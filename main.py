d56a124133ba172405e53df63fa8109ae594f26d
Julio Facal <starlyn.dev@gmail.com>
2022-10-17 21:29:35 -0300

    usando o find() para separar a url base dos parametros

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidad
 
 print(url)
 
-url_base = url[0:27]
+interrogacao = url.find('?')
+
+url_base = url[:interrogacao]
 print(url_base)
 
-url_parametros = url[28:]
+url_parametros = url[interrogacao + 1:]
 print(url_parametros)
