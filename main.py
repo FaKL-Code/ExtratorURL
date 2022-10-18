36b9369c89c399179fb363335841bceb7b3ca0a1
Julio Facal <starlyn.dev@gmail.com>
2022-10-18 10:02:30 -0300

    sanitizando a url

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+# url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+url = ""
+
+# Sanitização da URL
+url = url.replace(" ", "")
+
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
 
 # Separa base e parâmetros
 indice_interrogacao = url.find('?')
