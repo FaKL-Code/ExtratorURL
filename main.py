885a63c068a2e8b265397032179c87931c79b988
Julio Facal <starlyn.dev@gmail.com>
2022-10-17 21:24:20 -0300

    aprendendo a fatiar uma string

---
+++ main.py
@@ -0,0 +1,9 @@
+url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
+
+print(url)
+
+url_base = url[0:27]
+print(url_base)
+
+url_parametros = url[28:]
+print(url_parametros)
